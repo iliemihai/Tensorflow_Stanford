345b37bf84bfbc976ea1de218c165d7ef619ae4a
iliemihai <ily_sin_mike@yahoo.com>
2018-06-20 19:12:44 +0300

    Added code for learning tf

--- execute.sh
+++ execute.sh
@@ -1,2 +1,3 @@
+python home_liniar.py > RESULTS.csv
 awk -F'\t' -v OFS=',' ' NR == 1461 {print "Id", $0; next}   {print (NR+1460), $0}  ' RESULTS.csv >  RESULTSS.csv
 echo -e "Id,SalePrice\n$(cat RESULTSS.csv)" > RESULTSS.csv
